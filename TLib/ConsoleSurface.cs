463bf7fd93353a9a10e9f6a5f25a9171aeeec547
Timothy Hinchcliffe <Timothy.Hinchcliffe@secret>
2019-04-13 13:55:12 +0100

    Create classes to manage surfaces as consoles.

---
+++ TLib/ConsoleSurface.cs
@@ -0,0 +1,137 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class ConsoleSurface : MultiSurface
+        {
+            public class KeyBuilder
+            {
+                private readonly List<string> parts;
+                public const string SURFACEDISPLAYNAME = "[displayname]";
+                public const string SURFACEIDNAME = "[idname]";
+                public const string SURFACEPOS = "[pos]";
+
+                public KeyBuilder() { parts = new List<string>(); }
+                public KeyBuilder(string part1, string part2) : this() { Add(part1).Add(part2); }
+
+                public KeyBuilder Add(string part) { parts.Add(part); return this; }
+
+                public string Build(string displayname, string idname, int pos)
+                {
+                    string ret = "";
+                    parts.ForEach(delegate (string s)
+                    {
+                        if (s == SURFACEIDNAME)
+                            ret += idname;
+                        else if (s == SURFACEDISPLAYNAME)
+                            ret += displayname;
+                        else if (s == SURFACEPOS)
+                            ret += pos.ToString();
+                        else
+                            ret += s;
+                    });
+                    return ret;
+                }
+            }
+
+            public class SurfaceConfigFilter
+            {
+
+                private readonly string section;
+                private readonly KeyBuilder keypattern;
+
+                public string Section => section;
+
+                public string Key(string displayname, string idname, int pos) => keypattern.Build(displayname, idname, pos);
+
+                public SurfaceConfigFilter(string configSection, KeyBuilder configKey)
+                {
+                    section = configSection;
+                    keypattern = configKey;
+                }
+
+                public List<IMyTextSurface> Surfaces(IMyTextSurfaceProvider block)
+                {
+                    List<IMyTextSurface> surfaces = new List<IMyTextSurface>();
+                    Config.ConfigSection config = Config.Section((IMyTerminalBlock)block, section);
+                    for (int i = 0; i < block.SurfaceCount; i++)
+                    {
+                        IMyTextSurface surface = block.GetSurface(i);
+                        if (config.Get(Key(surface.DisplayName, surface.Name, i), false))
+                            surfaces.Add(surface);
+                    }
+                    return surfaces;
+                }
+            }
+
+            protected Program Me;
+            protected bool doEcho;
+            private bool ready;
+
+            public ConsoleSurface() : base() { ready = false; }
+
+            // These may conflict with IMyProgrammingBlock etc:
+            //public ConsoleSurface(IMyTextSurface surface) : base(surface) { }
+            //public ConsoleSurface(List<IMyTextSurface> newsurfaces) : base(newsurfaces) { }
+            //public ConsoleSurface(IMyTextSurfaceProvider provider, string surface) : base(provider, surface) { }
+            //public ConsoleSurface(IMyTextSurfaceProvider provider, int surface) : base(provider, surface) { }
+            //public ConsoleSurface(List<IMyTextSurfaceProvider> providers, string surface) : base(providers, surface) { }
+            //public ConsoleSurface(List<IMyTextSurfaceProvider> providers, int surface) : base(providers, surface) { }
+
+            public ConsoleSurface(IMyProgrammableBlock program, bool useConsole = true) : this()
+            {
+                Me = (Program)program;
+                doEcho = useConsole;
+            }
+            public ConsoleSurface(IMyProgrammableBlock program, int surfaceno, bool useConsole = true) : this(program, useConsole)
+            {
+                Add((IMyTextSurfaceProvider)program, surfaceno);
+            }
+            public ConsoleSurface(IMyProgrammableBlock program, string surfacename, bool useConsole = true) : this(program, useConsole)
+            {
+                Add((IMyTextSurfaceProvider)program, surfacename);
+            }
+            public ConsoleSurface(IMyProgrammableBlock program, List<IMyTextSurfaceProvider> providers, SurfaceConfigFilter filter, bool useConsole = true) : this(program, useConsole)
+            {
+                providers.ForEach(delegate (IMyTextSurfaceProvider provider) { surfaces.AddList(filter.Surfaces(provider)); });
+            }
+
+            protected void InitSurfaces()
+            {
+                foreach (IMyTextSurface surface in surfaces)
+                {
+                    surface.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
+                    surface.WriteText("", false);
+                }
+            }
+
+            public void Echo(string msg)
+            {
+                if (doEcho)
+                    Me.Echo(msg);
+                foreach (IMyTextSurface surface in surfaces)
+                {
+                    surface.WriteText(msg + "\n", true);
+                }
+            }
+        }
+    }
+}
---
+++ TLib/MultiSurface.cs
@@ -0,0 +1,145 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+using VRage.Game.GUI.TextPanel;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class MultiSurface : IMyTextSurface, IEnumerable
+        {
+            protected List<IMyTextSurface> surfaces;
+
+            public MultiSurface() { }
+            public MultiSurface(IMyTextSurface surface) { Add(surface); }
+            public MultiSurface(List<IMyTextSurface> newsurfaces) { Add(newsurfaces); }
+            public MultiSurface(IMyTextSurfaceProvider provider,string surface) { Add(provider, surface); }
+            public MultiSurface(IMyTextSurfaceProvider provider, int surface) { Add(provider, surface); }
+            public MultiSurface(List<IMyTextSurfaceProvider> providers,string surface) { Add(providers, surface); }
+            public MultiSurface(List<IMyTextSurfaceProvider> providers, int surface) { Add(providers, surface); }
+
+            /// <remarks>
+            /// Why isn't IMyTextSurfaceProvider and IEnumerable?
+            /// </remarks>
+            protected static List<IMyTextSurface> ProviderSurfaces(IMyTextSurfaceProvider provider)
+            {
+                List<IMyTextSurface> ret = new List<IMyTextSurface>();
+                for (int i = 0; i < provider.SurfaceCount; i++)
+                {
+                    ret.Add(provider.GetSurface(i));
+                }
+                return ret;
+            }
+
+            public void Add(IMyTextSurface surface) => surfaces.Add(surface);
+            public void Add(List<IMyTextSurface> newsurfaces) => surfaces.AddList(newsurfaces);
+            public void Add(IMyTextSurfaceProvider provider, string surface) => surfaces.AddList(ProviderSurfaces(provider).FindAll(s => s.Name == surface));
+            public void Add(IMyTextSurfaceProvider provider, int surface) => surfaces.Add(provider.GetSurface(surface));
+            public void Add(List<IMyTextSurfaceProvider> providers, string surface) => providers.ForEach(delegate (IMyTextSurfaceProvider p) { Add(p, surface); });
+            public void Add(List<IMyTextSurfaceProvider> providers, int surface) => providers.ForEach(delegate (IMyTextSurfaceProvider p) { Add(p, surface); });
+
+            public float FontSize { get { return surfaces.First().FontSize; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.FontSize = value; }); } }
+            public Color FontColor { get { return surfaces.First().FontColor; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.FontColor = value; }); } }
+            public Color BackgroundColor { get { return surfaces.First().BackgroundColor; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.BackgroundColor = value; }); } }
+            public byte BackgroundAlpha { get { return surfaces.First().BackgroundAlpha; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.BackgroundAlpha = value; }); } }
+            public float ChangeInterval { get { return surfaces.First().ChangeInterval; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.ChangeInterval = value; }); } }
+            public string Font { get { return surfaces.First().Font; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.Font = value; }); } }
+            public TextAlignment Alignment { get { return surfaces.First().Alignment; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.Alignment = value; }); } }
+            public string Script { get { return surfaces.First().Script; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.Script = value; }); } }
+            public ContentType ContentType { get { return surfaces.First().ContentType; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.ContentType = value; }); } }
+
+            public Vector2 SurfaceSize { get { throw new MethodNotAvailable(); } }
+
+            public Vector2 TextureSize { get { throw new MethodNotAvailable(); } }
+
+            public bool PreserveAspectRatio { get { return surfaces.First().PreserveAspectRatio; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.PreserveAspectRatio = value; }); } }
+            public float TextPadding { get { return surfaces.First().TextPadding; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.TextPadding = value; }); } }
+            public Color ScriptBackgroundColor { get { return surfaces.First().ScriptBackgroundColor; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.ScriptBackgroundColor = value; }); } }
+            public Color ScriptForegroundColor { get { return surfaces.First().ScriptForegroundColor; } set { surfaces.ForEach(delegate (IMyTextSurface s) { s.ScriptForegroundColor = value; }); } }
+
+            public string Name { get { throw new MethodNotAvailable(); } }
+
+            public string DisplayName { get { throw new MethodNotAvailable(); } }
+
+            public string CurrentlyShownImage { get { throw new MethodNotAvailable(); } }
+
+            public void AddImagesToSelection(List<string> ids, bool checkExistence = false) => surfaces.ForEach(delegate (IMyTextSurface s) { s.AddImagesToSelection(ids, checkExistence); });
+
+            public void AddImageToSelection(string id, bool checkExistence = false) => surfaces.ForEach(delegate (IMyTextSurface s) { s.AddImageToSelection(id, checkExistence); });
+
+            public void ClearImagesFromSelection() => surfaces.ForEach(delegate (IMyTextSurface s) { s.ClearImagesFromSelection(); });
+
+            public MySpriteDrawFrame DrawFrame()
+            {
+                throw new MethodNotAvailable();
+            }
+
+            public IEnumerator GetEnumerator() => ((IEnumerable)surfaces).GetEnumerator();
+
+            public void GetFonts(List<string> fonts) => surfaces.First().GetFonts(fonts);
+
+            public void GetScripts(List<string> scripts) => surfaces.First().GetScripts(scripts);
+
+            public void GetSelectedImages(List<string> output)
+            {
+                throw new MethodNotAvailable();
+            }
+
+            public void GetSprites(List<string> sprites) => surfaces.First().GetSprites(sprites);
+
+            public string GetText()
+            {
+                throw new MethodNotAvailable();
+            }
+
+            public Vector2 MeasureStringInPixels(StringBuilder text, string font, float scale) => surfaces.First().MeasureStringInPixels(text, font, scale);
+
+
+            public void ReadText(StringBuilder buffer, bool append = false)
+            {
+                throw new MethodNotAvailable();
+            }
+
+            public void RemoveImageFromSelection(string id, bool removeDuplicates = false) => surfaces.First().RemoveImageFromSelection(id, removeDuplicates);
+
+            public void RemoveImagesFromSelection(List<string> ids, bool removeDuplicates = false) => surfaces.First().RemoveImagesFromSelection(ids, removeDuplicates);
+
+            public bool WriteText(string value, bool append = false)
+            {
+                bool allOk = true;
+                surfaces.ForEach(delegate (IMyTextSurface s)
+                {
+                    if (!s.WriteText(value, append))
+                        allOk = false;
+                });
+                return allOk;
+            }
+
+            public bool WriteText(StringBuilder value, bool append = false)
+            {
+                bool allOk = true;
+                surfaces.ForEach(delegate (IMyTextSurface s)
+                {
+                    if (!s.WriteText(value, append))
+                        allOk = false;
+                });
+                return allOk;
+            }
+        }
+    }
+}
