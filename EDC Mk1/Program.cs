64a14e5597752b48c874550b414ee525a0b5bd7a
Timothy Hinchcliffe <Timothy.Hinchcliffe@secret>
2020-07-13 12:19:27 +0100

    Write full config to lights on initial pass.

--- EDC Mk1/Program.cs
+++ EDC Mk1/Program.cs
@@ -360,18 +360,20 @@ namespace IngameScript {
                 lconf.Section("EDC").Default("Save Config", false);
                 lconf.Section("EDC").SetComment("Save Config", "Set this to 'true' to save out all properties with their current values");
                 string statename;
-                if (block.CustomData == "" && Utility.IsType<IMyTimerBlock>(block)) {
-                    lconf.Section("DEPRESSURIZED").Default("1", "TriggerNow");
-                    lconf.Section("PRESSURIZED").Default("1", "TriggerNow");
+                if (block.CustomData == "") {
+                    if (Utility.IsType<IMyTimerBlock>(block)) {
+                        lconf.Section("DEPRESSURIZED").Default("1", "TriggerNow");
+                        lconf.Section("PRESSURIZED").Default("1", "TriggerNow");
+                    }
+                    if (Utility.IsType<IMyLightingBlock>(block)) {
+                        lconf.Section("DEPRESSURIZED").Default("OnOff", true);
+                        lconf.Section("PRESSURIZED").Default("OnOff", false);
+                    }
                 }
                 if (counts.ocomps > 0 || counts.dcomps > 0) {
                     section = lconf.Section(statename = "DEPRESSURIZED");
-                    if (Utility.IsType<IMyLightingBlock>(block))
-                        section.Set("OnOff", section.Get("OnOff", true));
                 } else if (counts.pcomps > 0) {
                     section = lconf.Section(statename = "PRESSURIZED");
-                    if (Utility.IsType<IMyLightingBlock>(block))
-                        section.Set("OnOff", section.Get("OnOff", false));
                 } else {
                     section = lconf.Section(statename = "UNKNOWN");
                 }
